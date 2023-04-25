db787f1c530632da5d9753ed8e57b56d926c6eb6
Felipe Madruga Gusmão <felipe.mg@dinfor.uniceub>
2023-04-25 08:03:51 -0300

    solucoes

--- q4/q4.go
+++ q4/q4.go
@@ -1,6 +1,36 @@
 package q4
 
+import "errors"
+
 func CalculateFinalPrice(basePrice float64, state string, taxCode int) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	if basePrice <= 0 {
+		return 0, errors.New("preço base inválido")
+	}
+	var imposto, frete float64
+
+	if taxCode == 1 {
+		imposto = 0.05
+	} else if taxCode == 2 {
+		imposto = 0.1
+	} else if taxCode = 3 {
+		imposto = 0.15
+	} else {
+		return 0, errors.New("imposto não encontrado")
+	}
+
+	if state == "SP" {
+		frete = 0.1
+	} else if state == "RJ" {
+		frete = 0.2
+	} else if state == "MG" {
+		frete = 0.2
+	} else if state == "ES" {
+		frete = 0.25
+	} else {
+		frete = 0.3
+	}
+
+	precoFinal := basePrice + basePrice*imposto + basePrice*frete
+
+	return precoFinal, nil
 }
--- q5/q5.go
+++ q5/q5.go
@@ -1,6 +1,10 @@
 package q5
 
 func ConvertTemperature(temp float64, fromScale string, toScale string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	formulas := map[string]map[string]func(float64) float64{
+
+		"C": {
+			"F": func(c float64) float64 { return c*9/5 = 32 },
+		}
+	}
 }
